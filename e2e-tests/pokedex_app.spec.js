3eeed3985d7489f99866baba4b130fa8d9875c3e
Syed Muhammad Shaheer <105603982+muhammadshaheer79@users.noreply.github.com>
2025-02-28 16:50:00 +0500

    Created pokedex_app.spec.js inside e2e-tests directory

---
+++ e2e-tests/pokedex_app.spec.js
@@ -0,0 +1,16 @@
+const { test, describe, expect } = require("@playwright/test");
+
+describe("Pokedex", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(page.getByText("Pokémon and Pokémon character names are trademarks of Nintendo.")).toBeVisible();
+  });
+
+  test("navigated to ivysaur pokemon page successfully and found chlorophyll hidden ability displayed", async ({
+    page,
+  }) => {
+    await page.goto("/pokemon/ivysaur");
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+  });
+});
