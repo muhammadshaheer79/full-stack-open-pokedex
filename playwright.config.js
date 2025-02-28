ef85afced006c7200692b6425ee417266c8bc993
Syed Muhammad Shaheer <105603982+muhammadshaheer79@users.noreply.github.com>
2025-02-28 16:47:38 +0500

    Created playwright.config.js
    Configured a Playwright development server to start the app

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e-tests',
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:8080',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:8080/',
+  },
+});
