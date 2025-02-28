99945f7d91d9710ea7ed386d81d7bdaa338994bb
Syed Muhammad Shaheer <105603982+muhammadshaheer79@users.noreply.github.com>
2025-02-28 17:01:35 +0500

    Updated playwright.config.js resolved linting issues

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './e2e-tests',
@@ -11,4 +11,4 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:8080/',
   },
-});
+})
